41832dde2b8a441a5d40d33d33a27abc9dac0851
tarchan <tarchan@gmail.com>
2014-05-26 17:25:47 +0900

    PNGフォーマットで保存する機能を追加

--- src/com/mac/tarchan/paintfx/PaintFXController.java
+++ src/com/mac/tarchan/paintfx/PaintFXController.java
@@ -6,17 +6,28 @@
 
 package com.mac.tarchan.paintfx;
 
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javafx.embed.swing.SwingFXUtils;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Point2D;
 import javafx.scene.Group;
+import javafx.scene.SnapshotParameters;
 import javafx.scene.canvas.Canvas;
 import javafx.scene.canvas.GraphicsContext;
 import javafx.scene.control.Label;
+import javafx.scene.image.Image;
+import javafx.scene.image.WritableImage;
 import javafx.scene.input.MouseEvent;
+import javafx.stage.FileChooser;
+import javax.imageio.ImageIO;
 
 /**
  * PaintFXController
@@ -25,12 +36,38 @@ import javafx.scene.input.MouseEvent;
  */
 public class PaintFXController implements Initializable {
     
+    private static final Logger logger = Logger.getLogger(PaintFXController.class.getName());
     private Canvas canvas;
+    private final FileChooser fileChooser = new FileChooser();
+    private File savedFile;
     @FXML
     private Group group;
     @FXML
     private Label status;
-    
+
+    private void saveImage(Image image, File file) throws IOException {
+        BufferedImage saveImage = SwingFXUtils.fromFXImage(image, null);
+        ImageIO.write(saveImage, "png", file);
+    }
+
+    private void saveFile(File file) {
+        if (file == null) {
+            file = fileChooser.showSaveDialog(group.getScene().getWindow());
+            if (file == null) {
+                // キャンセル
+                return;
+            }
+        }
+        try {
+            SnapshotParameters params = new SnapshotParameters();
+            WritableImage image = canvas.snapshot(params, null);
+            saveImage(image, file);
+            savedFile = file;
+        } catch (IOException ex) {
+            logger.log(Level.SEVERE, "イメージを保存できません。: " + file, ex);
+        }
+    }
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
         canvas = new Canvas(1024, 1024);
@@ -61,6 +98,7 @@ public class PaintFXController implements Initializable {
 
     @FXML
     private void onSave(ActionEvent event) {
+        saveFile(null);
     }
 
     @FXML
